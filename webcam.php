648d064d4a6907deecdb290c90453b3497ad62bf
remyauda <remyauda@yahoo.fr>
2017-04-27 22:37:14 +0200

    [ADD] Initial commit.

---
+++ webcam.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="description" content="webcam visualistion" />
+<meta name="keywords" content="webcam, visualisation" />
+<meta name="author" content="AUDA">
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="webcam.css">
+<title>webcam</title>
+</head>
+<body >
+  <video width=200 height=200 id="video" controls autoplay></video>
+</body>
+<script type="text/javascript" src="webcam.js"></script>
+</html>
